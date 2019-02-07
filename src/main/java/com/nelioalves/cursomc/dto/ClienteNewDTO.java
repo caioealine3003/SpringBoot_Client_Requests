3b57bb23c119dee6096835b3cf8717a54e6e5ee6
Caio Sampaio Pires <caiosam3003@outlook.com>
2019-02-06 23:04:49 -0200

    Adicionando senha a cliente.

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -23,7 +23,11 @@ public class ClienteNewDTO implements Serializable{
 	
 	@NotEmpty(message="Preenchimento obrigatório.")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
+	
+	@NotEmpty
+	private String senha;
 	
 	@NotEmpty(message="Preenchimento obrigatório.")
 	private String logradouro;
@@ -151,6 +155,14 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 	
